43dc59aee9cbbd582fd6a6a7a75a577c2a78d51b
Aditya Jayant Gupta <daemonad@protonmail.com>
2022-11-03 16:59:27 +0530

    Add slide-in animation + particle effects

---
+++ docs/js/index.js
@@ -0,0 +1,74 @@
+(function ($) {
+  /**
+   * Copyright 2012, Digital Fusion
+   * Licensed under the MIT license.
+   * http://teamdf.com/jquery-plugins/license/
+   *
+   * @author Sam Sehnert
+   * @desc A small plugin that checks whether elements are within
+   *     the user visible viewport of a web browser.
+   *     only accounts for vertical position, not horizontal.
+   */
+
+  $.fn.visible = function (partial) {
+    var $t = $(this),
+      $w = $(window),
+      viewTop = $w.scrollTop(),
+      viewBottom = viewTop + $w.height(),
+      _top = $t.offset().top,
+      _bottom = _top + $t.height(),
+      compareTop = partial === true ? _bottom : _top,
+      compareBottom = partial === true ? _top : _bottom;
+
+    return compareBottom <= viewBottom && compareTop >= viewTop;
+  };
+})(jQuery);
+
+$(window).scroll(function (event) {
+  $(".come-in-left").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in-left-animation");
+    }
+  });
+  $(".come-in-right").each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in-right-animation");
+    }
+  });
+});
+
+var win = $(window);
+var leftCards = $(".come-in-left");
+var rightCards = $(".come-in-right");
+
+// Already visible modules
+leftCards.each(function (i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+});
+
+rightCards.each(function (i, el) {
+  var el = $(el);
+  if (el.visible(true)) {
+    el.addClass("already-visible");
+  }
+});
+
+win.scroll(function (event) {
+  leftCards.each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in-left-animation");
+    }
+  });
+  rightCards.each(function (i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in-right-animation");
+    }
+  });
+});
